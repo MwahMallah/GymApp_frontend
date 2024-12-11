426b34fd719d4600a0cae4ec0dc38fa2d7bfde64
MwahMallah <maximus27rus@gmail.com>
2024-12-11 22:35:09 +0100

    renamed exercise section to DataSection. added new library.

---
+++ src/components/DataSection/DataSection.jsx
@@ -0,0 +1,12 @@
+
+import TrackingHistory from "../TrackingHistory/TrackingHistory";
+
+function DataSection() {
+    return (
+        <div className="card col-span-3 rounded-3xl">
+            <TrackingHistory />
+        </div>
+    )
+}
+
+export default DataSection
