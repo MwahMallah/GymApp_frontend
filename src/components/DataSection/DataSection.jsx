392161b2a9cf2a1862598b4acdefb79b8bc02722
MwahMallah <maximus27rus@gmail.com>
2024-12-12 21:25:39 +0100

    Enhanced visuals of graph.

--- src/components/DataSection/DataSection.jsx
+++ src/components/DataSection/DataSection.jsx
@@ -3,8 +3,15 @@ import TrackingHistory from "../TrackingHistory/TrackingHistory";
 
 function DataSection() {
     return (
-        <div className="card col-span-3 rounded-3xl">
-            <TrackingHistory />
+        <div className="card col-span-3 rounded-3xl flex flex-col h-full gap-2">
+            <div className="flex-grow-[1] border-2 rounded-3xl">
+                <TrackingHistory />
+            </div>
+            <div className="flex-grow-[1]">
+                <div className="bg-gray-600 h-[200px] w-full">
+
+                </div>
+            </div>
         </div>
     )
 }
